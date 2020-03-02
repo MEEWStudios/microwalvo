568b4cccfae6d197e09a72ad3914e92a35c01d50
Willard Peralta <willardp808@gmail.com>
2020-03-01 18:07:32 -1000

    spotlight detection

---
+++ Assets/Scripts/Detection/log_find.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class log_find : MonoBehaviour
+{
+
+    void Awake()
+    {
+        if(GameObject.Find("Ronaldo") != null)
+        {
+            Debug.Log("Ronaldo exists.");
+        }
+        else 
+        {
+            Debug.Log("Ronaldo doesn't exist.");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
