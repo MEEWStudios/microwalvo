decdac1813b488ce64e17d8933d9bd1a4fb9c427
Ethan <epchow@hawaii.edu>
2020-03-25 18:06:14 -1000

    Optimize blur script, adjust camera angle, add timer example

--- Assets/Scripts/DrawOnTexture.cs
+++ Assets/Scripts/DrawOnTexture.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,22 +8,27 @@ public class DrawOnTexture : MonoBehaviour {
 	public Camera cam;
 	public Color drawColor;
 	public Renderer destinationRenderer;
-	public int TextureSize;
-	public int Radius;
-	public Color BlurColor;
+	public int textureDensity;
+	public int radius;
+	public Color blurColor;
 	public GameObject spotlight1;
 	public GameObject spotlight2;
 
+	private int textureWidth;
+	private int textureHeight;
 	private Texture2D texture;
-
-
+	private Color[,] textureCache;
 
 	void Start() {
+		textureWidth = Mathf.FloorToInt(textureDensity * this.gameObject.transform.localScale.x + 0.5f);
+		textureHeight = Mathf.FloorToInt(textureDensity * this.gameObject.transform.localScale.z + 0.5f);
 
-		texture = new Texture2D(TextureSize, TextureSize, TextureFormat.RFloat, false, true);
-		for (int i = 0; i < texture.height; i++) {
-			for (int j = 0; j < texture.width; j++) {
-				texture.SetPixel(i, j, BlurColor);
+		texture = new Texture2D(textureWidth, textureHeight, TextureFormat.RFloat, false, true);
+		textureCache = new Color[textureWidth, textureHeight];
+		for (int i = 0; i < textureWidth; i++) {
+			for (int j = 0; j < textureHeight; j++) {
+				textureCache[i, j] = blurColor;
+				texture.SetPixel(i, j, blurColor);
 			}
 		}
 		texture.Apply();
@@ -36,23 +42,42 @@ public class DrawOnTexture : MonoBehaviour {
 		//Debug.Log(cam.WorldToScreenPoint(spotlight.transform.position).x);
 	}
 
-
-
-
 	void Update() {
-
 		// create a vector that uses x and y coordinates that were converted from world to screen coordinates
 		Vector3 spotlight1Vector = new Vector3(cam.WorldToScreenPoint(spotlight1.transform.position).x, cam.WorldToScreenPoint(spotlight1.transform.position).y, 0.0f);
-
 		Vector3 spotlight2Vector = new Vector3(cam.WorldToScreenPoint(spotlight2.transform.position).x, cam.WorldToScreenPoint(spotlight2.transform.position).y, 0.0f);
 
 		//the ray for the spotlight
 		Ray ray = cam.ScreenPointToRay(spotlight1Vector); //Input.mousePosition 
 		Ray ray2 = cam.ScreenPointToRay(spotlight2Vector);
 
-
 		RaycastHit hit;
 		RaycastHit hit2;
+
+		bool dirty = false;
+
+		//if (Physics.Raycast(ray, out hit, 100)) {
+		//	Color color = new Color(Time.timeSinceLevelLoad, 0, 0, 1);
+		//	int x = (int) (hit.textureCoord.x * texture.width);
+		//	int y = (int) (hit.textureCoord.y * texture.height);
+
+		//	if (textureCache[x, y] != color) {
+		//		dirty = true;
+		//		texture.SetPixel(x, y, color);
+		//	}
+
+		//	DateTime before = DateTime.Now;
+		//	List<Vector2> coordinates = GetCoordinatesWithinRadius(new Vector2(x, y), radius);
+		//	DateTime after = DateTime.Now;
+		//	Debug.Log(coordinates.Count);
+		//	Debug.Log(after - before);
+		//	foreach (Vector2 coordinate in coordinates) {
+		//		if (textureCache[(int) coordinate.x, (int) coordinate.y] != color) {
+		//			dirty = true;
+		//			texture.SetPixel((int) coordinate.x, (int) coordinate.y, color);
+		//		}
+		//	}
+		//}
 
 		//spotlight1
 		if (Physics.Raycast(ray, out hit, 100)) {
@@ -66,26 +91,25 @@ public class DrawOnTexture : MonoBehaviour {
 			int x = (int) (hit.textureCoord.x * texture.width);
 			int y = (int) (hit.textureCoord.y * texture.height);
 
-			texture.SetPixel(x, y, color);
-
-			for (int i = 0; i < texture.height; i++) {
-				for (int j = 0; j < texture.width; j++) {
-					float dist = Vector2.Distance(new Vector2(i, j), new Vector2(x, y));
-					if (dist <= Radius)
-						texture.SetPixel(i, j, color);
-				}
+			if (textureCache[x, y] != color) {
+				dirty = true;
+				texture.SetPixel(x, y, color);
 			}
 
-			texture.Apply();
-			destinationRenderer.material.SetTexture("_MouseMap", texture);
+			for (int i = 0; i < textureWidth; i++) {
+				for (int j = 0; j < textureHeight; j++) {
+					//float dist = Mathf.Sqrt(Mathf.Pow(x - i, 2) + Mathf.Pow(y - j, 2));
+					float dist = Vector2.Distance(new Vector2(i, j), new Vector2(x, y));
+					if (dist <= radius) {
+						if (textureCache[i, j] != color) {
+							dirty = true;
+							texture.SetPixel(i, j, color);
+							//break;
+						}
+					}
+				}
+			}
 		}
-
-
-
-
-
-
-
 
 		//spotlight2
 		if (Physics.Raycast(ray2, out hit2, 100)) {
@@ -99,16 +123,27 @@ public class DrawOnTexture : MonoBehaviour {
 			int x = (int) (hit2.textureCoord.x * texture.width);
 			int y = (int) (hit2.textureCoord.y * texture.height);
 
-			texture.SetPixel(x, y, color);
-
-			for (int i = 0; i < texture.height; i++) {
-				for (int j = 0; j < texture.width; j++) {
-					float dist = Vector2.Distance(new Vector2(i, j), new Vector2(x, y));
-					if (dist <= Radius)
-						texture.SetPixel(i, j, color);
-				}
+			if (textureCache[x, y] != color) {
+				dirty = true;
+				texture.SetPixel(x, y, color);
 			}
 
+			for (int i = 0; i < textureWidth; i++) {
+				for (int j = 0; j < textureHeight; j++) {
+					//float dist = Mathf.Sqrt(Mathf.Pow(x - i, 2) + Mathf.Pow(y - j, 2));
+					float dist = Vector2.Distance(new Vector2(i, j), new Vector2(x, y));
+					if (dist <= radius) {
+						if (textureCache[i, j] != color) {
+							dirty = true;
+							texture.SetPixel(i, j, color);
+							//break;
+						}
+					}
+				}
+			}
+		}
+
+		if (dirty) {
 			texture.Apply();
 			destinationRenderer.material.SetTexture("_MouseMap", texture);
 		}
@@ -116,4 +151,34 @@ public class DrawOnTexture : MonoBehaviour {
 	}
 
 
+
+	List<Vector2> GetCoordinatesWithinRadius(Vector2 origin, int radius) {
+		Stack<Vector2> open = new Stack<Vector2>();
+		open.Push(origin);
+		List<Vector2> closed = new List<Vector2>();
+		List<Vector2> result = new List<Vector2>();
+
+		while (open.Count > 0) {
+			Vector2 current = open.Pop();
+
+			if (closed.Contains(current)) {
+				continue;
+			} else {
+				closed.Add(current);
+			}
+
+			float distance = Vector2.Distance(current, origin);
+
+			if (distance < radius + 0.5) {
+				result.Add(current);
+
+				open.Push(new Vector2(current.x, current.y - 1));
+				open.Push(new Vector2(current.x + 1, current.y));
+				open.Push(new Vector2(current.x, current.y + 1));
+				open.Push(new Vector2(current.x - 1, current.y));
+			}
+		}
+
+		return result;
+	}
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+	private int time = 60;
+	private bool running = false;
+	private float timeCounter = 0;
+
+	// Start is called before the first frame update
+	void Start() {
+		Run();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (running) {
+			timeCounter += Time.deltaTime;
+			this.GetComponent<Text>().text = "Time: " + TimestampToString(time - timeCounter);
+		}
+	}
+
+	void Run() {
+		running = true;
+	}
+
+	string TimestampToString(float timestamp) {
+		int minutes = Mathf.FloorToInt(timestamp / 60);
+		timestamp -= minutes * 60;
+		int seconds = Mathf.FloorToInt(timestamp);
+
+		return minutes + ":" + (seconds < 10 ? "0" + seconds : "" + seconds);
+	}
+}
