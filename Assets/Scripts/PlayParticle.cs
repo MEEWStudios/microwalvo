60e8c3df93b0cb04783525f50fad9b19305bb9b6
Emily Pang <42831699+eppang@users.noreply.github.com>
2020-04-08 00:57:40 -1000

    Added particle effect and water cylinder to fountain

---
+++ Assets/Scripts/PlayParticle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticle : MonoBehaviour {
+	// Start is called before the first frame update
+	void Start() {
+		this.GetComponent<ParticleSystem>().Play();
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+}
