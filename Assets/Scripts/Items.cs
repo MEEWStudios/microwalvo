fa31a21edefeea5e84919f8c94bd51acfe9356dc
Ethan <epchow@hawaii.edu>
2020-04-15 20:05:25 -1000

    Improve item spawning, fix effect not displaying properly

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -2,41 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Items : MonoBehaviour
-{
+public class Items : MonoBehaviour {
 	public GameObject goodItem;
 	public int goodItemCount;
 	public GameObject badItem;
 	public int badItemCount;
 	public GameObject animation;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+	// Start is called before the first frame update
+	void Start() {
 		SpawnItems();
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update() {
+
+	}
 
 	void SpawnItems() {
-		Vector3 position = goodItem.transform.position;
 		for (int i = 0; i < goodItemCount; i++) {
-			position.x = Random.Range(-80, 80);
-			position.z = Random.Range(-80, 80);
+			Vector3 position = GameManager.GetRandomPointOnMap(goodItem.transform.position.y);
 			GameObject GoodItem = Instantiate(goodItem, position, Quaternion.identity) as GameObject;
-			GameObject sparkle = Instantiate(animation, transform.position, Quaternion.identity) as GameObject;
+			GameObject sparkle = Instantiate(animation, position, Quaternion.identity, GoodItem.transform) as GameObject;
+			sparkle.transform.localScale = new Vector3(2, 2, 2);
 		}
 
-		position = badItem.transform.position;
 		for (int i = 0; i < badItemCount; i++) {
-			position.x = Random.Range(-80, 80);
-			position.z = Random.Range(-80, 80);
+			Vector3 position = GameManager.GetRandomPointOnMap(goodItem.transform.position.y);
 			GameObject BadItem = Instantiate(badItem, position, Quaternion.identity) as GameObject;
-			GameObject sparkle2 = Instantiate(animation, transform.position, Quaternion.identity) as GameObject;
+			GameObject sparkle = Instantiate(animation, position, Quaternion.identity, BadItem.transform) as GameObject;
+			sparkle.transform.localScale = new Vector3(2, 2, 2);
 		}
 	}
 }
