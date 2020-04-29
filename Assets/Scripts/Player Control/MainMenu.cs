00210895fea096ffe6092d7591af9f3d03873acf
Emily Pang <42831699+eppang@users.noreply.github.com>
2020-04-28 20:17:18 -1000

    Implemented tutorial screen

--- Assets/Scripts/Player Control/MainMenu.cs
+++ Assets/Scripts/Player Control/MainMenu.cs
@@ -4,10 +4,15 @@ using System.Collections;
 
 public class MainMenu : PlayerControl {
 
-	public GameObject menu;
+	public GameObject mainMenu;
+	public GameObject panel;
+	public GameObject backText;
 	public Button play;
 	public Button instruct;
 	public Button quit;
+	Image imageComponent;
+	public Sprite title;
+	public Sprite tutorial;
 
 	// Use this for initialization
 	void Start() {
@@ -15,14 +20,25 @@ public class MainMenu : PlayerControl {
 	}
 
 	public override void ProcessGamepadInput(EZGamepad gamepad) {
-
+		imageComponent = GetComponent<Image>();
 		// Start game
 		if (gamepad.buttonSouth.justPressed) {
-			menu = GameObject.Find("MainMenu");
-			menu.SetActive(false);
+			mainMenu = GameObject.Find("MainMenu");
+			mainMenu.SetActive(false);
 			GameManager.ResetRound();
 			ScoreManager.ResetScores();
 			GameManager.StartRound();
+		}
+		if (gamepad.buttonWest.justPressed) {
+			backText.SetActive(true);
+			imageComponent.sprite = tutorial;
+			panel.SetActive(false);
+		}
+		if (gamepad.buttonEast.justPressed && imageComponent.sprite == tutorial) {
+			Debug.Log("button pressed!");
+			panel.SetActive(true);
+			backText.SetActive(false);
+			imageComponent.sprite = title;
 		}
 		if (gamepad.buttonEast.justPressed) {
 			Application.Quit();
